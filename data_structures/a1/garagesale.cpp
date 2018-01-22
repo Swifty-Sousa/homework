29dce6dea1753e83079f6dc97810d1b875b8c9fd
SwiftyX3 <sousachristian440@gmail.com>
2018-01-22 14:45:09 -0700

    updated a1

--- data_structures/a1/garagesale.cpp
+++ data_structures/a1/garagesale.cpp
@@ -2,26 +2,62 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
-
-void create()
+// Split is a a fuction I wrote for a previous class
+// you can see it is in my resources repo on my github
+//https://github.com/swiftsong/resources/blob/master/cppresources.cpp
+int Split(string str, char x ,string array[])
 {
-    fstream datafile;
-    datafile.open("garagesale.txt");
-    if (datfile.fail())
+    int i=0;
+    int j=0;
+    int k=0;
+    int c=0;
+    int count=0;
+    if (str=="")
+        {
+            return 0;
+        }
+    str= str + x;
+    while(i<str.length())
     {
-        cout<< "Error garagesale textfile not found"<< endl;
+        if(str[i]==x)
+        {
+            array[c]=str.substr(j,i-j);
+            j=i+1;
+            c++;
+            count++;
+        }
+        i++;
     }
-    string holder;
+    return count;
+}
+
+// this fuction takes the data array and fills by reference
+// using the file garagesale.txt
+void getdata(string data[])
+{
+    string line;
+    fstream datafile;
+    int i=0;
+    datafile.open("garageSale.txt");
+    if (datafile.fail())
+    {
+        cout<< "Error, file not found"<<endl;
+    }
     while(!datafile.eof())
     {
-        getline(datafile,holder);
-        
+        getline(datafile, line);
+        data[i]=line;
+        i++;
     }
+
 }
 
 
 
-int main void()
+int main(void)
 {
-    struct objects[100];
+    string data[100];
+    //struct objects[100];
+    getdata(data);
+    cout<< data[5]<< endl;
 }
