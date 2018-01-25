6140a486e57648f93dc41353b438ff7efbb3a296
SwiftyX3 <sousachristian440@gmail.com>
2018-01-25 09:50:17 -0700

    updated a1

--- data_structures/a1/garagesale.cpp
+++ data_structures/a1/garagesale.cpp
@@ -8,14 +8,13 @@ struct item{
     string name; // name of the object being bought or sold
     int price; // the price or price to pay of the object
     item(){}
-    item(int n, int s, string na)
+    item(string na, int s, int p)
     {
         status=s;
         name=na;
-        price=n;    
+        price=p;    
     }
 };
-
 // Split is a a fuction I wrote for a previous class
 // you can see it is in my resources repo on my github
 //https://github.com/swiftsong/resources/blob/master/cppresources.cpp
@@ -30,6 +29,7 @@ int Split(string str, char x ,string array[])
             return 0;
         }
     str= str + x;
+    int count=0;
     while(i<str.length())
     {
         if(str[i]==x)
@@ -40,57 +40,77 @@ int Split(string str, char x ,string array[])
             count++;
         }
         i++;
+    }
     return count;
 }
-
-// this fuction takes the data array and fills by reference
-// using the file garagesale.txt
-void getdata(string data[])
+int delete_array_pos(array[],index,size)// fuction that removes a entry in the array
 {
-    string line;
-    fstream datafile;
-    int i=0;
-    datafile.open("garageSale.txt");
-    if (datafile.fail())
-    while(!datafile.eof())
+    for(i=index;i<size;i++)
     {
-        getline(datafile, line);
-        data[i]=line;
-        i++;
+        array[i]=array[x+1];
+        size--;
     }
+    return size;
+}
+bool evaluate(item dd, int index)
+{
+    for
+
 
 }
- void processdata(string data[100], item objects[100])
- {
-     string holder_array[3];
-     int status;
-     string name;
-     int cost;
-     string holder_str;
-     for(int i=0; i<100; i++)
-     {
-        Split(data[i], ',', holder_array);
-        name=holder_array[0];
-        if(holder_array[1]== " for sale")
-        {
-            status=1;
-        }
-        else
-        {
-            status=2;
-        }
-        holder_str= holder_array[2].substr(1,holder_array[2].length());
-     }
-
- }
-
-
-
-int main(void)
+item makeitem(holder_array)
 {
-    string data[100];
+    int status;
+    string name;
+    int cost;
+    string holder_str;
+    name=holder_array[0];
+    if(holder_array[1]== " for sale")
+    {
+        status=1;
+    }
+    else
+    {
+        status=2;
+    }
+    holder_str= holder_array[2].substr(1,holder_array[2].length());// this is the strign that will hold the price
+    holderint=stoi(holder_str);
+    if(status==1)
+    {
+        holderint=holdering*(-1); // this will come up later to help determin a price match
+    }
+    item a(name,status,stoi(holder_str))
+    return a;
+
+}
+// this fuction reads each line parses it and then creates a struct with the line
+// then the struct is passes to evaluate
+void getdata(string filename, item objects)
+{
+     string line;
+     string data[3];
+     fstream datafile;
+     int i=0;
+     datafile.open(filename);
+     if (datafile.fail())
+     while(!datafile.eof())
+     {
+         getline(datafile, line);
+         Split(line,',',data)
+         item holder=makeitem(data[]);
+         if(evaluate(holder,i, objects)==false)
+         {
+             objects[i]=holder;
+         }
+         i++;
+     }
+     datafile.close();
+}
+
+int main(int argc, char *argv[])
+{
     item objects[100];
-    getdata(data);
-    processdata(data,objects);
+    string filename= argv[1];
+    getdata(filename,objects);
     //cout<< data[53]<< endl;
 }
