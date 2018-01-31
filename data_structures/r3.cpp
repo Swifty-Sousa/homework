a2668e118198b8d27d1d7767d8a4f92e86d44a7c
SwiftyX3 <sousachristian440@gmail.com>
2018-01-31 09:55:58 -0700

    created r3

--- data_structures/r3.cpp
+++ data_structures/r3.cpp
@@ -1,6 +1,57 @@
 // author: Christian F. Sousa
 // CSCI 2270: data structures, section 104
+#include<iostream>
+#include<fstream>
+using namespace std;
+void doublearray(int array, int *s)
+{
+    int capacity=*s *2;
+    int *new_array;
+    new_array=new int[capacity];
+    for(int i=0;i<*s;i++)
+    {
+        new_array[i]=*array[i];
+    }
+    new_array=&array;
+    //delete [] array;
+    *s=capacity;
+}
 int main(int argc, char *argv[])
 {
-    
+    int array[10];
+    for( int i=0; i<10;i++)
+    {
+        array[i]=-10;
+    }
+    int *arrayptr=array;
+    if(argc!=2)
+    {
+        cout<<"Error code 1"<< endl;
+    }
+    string filename=argv[1];
+    fstream datafile;
+    int i=0;
+    int size=5;
+    string line;
+    datafile.open(filename);
+    if(datafile.fail())
+    {
+        cout<< "file not found"<< endl;
+    }
+    while(!datafile.eof())
+    {
+        //cout<< "we are at getline"<< endl;
+    getline(datafile, line);
+        if(array[i]!=-10)
+        {            
+            doublearray(array,&size);
+            array[i]=stoi(line);
+        }
+        else
+        {
+            array[i]=stoi(line);
+        }
+    i++;
+    }
+    datafile.close();
 }
