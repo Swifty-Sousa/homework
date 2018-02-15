bf933a873f282d42bd1bf0db8c5e29b458c1912f
SwiftyX3 <sousachristian440@gmail.com>
2018-02-15 13:20:45 -0700

    created a4

---
+++ data_structures/a4/Assignment4.cpp
@@ -0,0 +1,51 @@
+//Author: Christian F. Sousa
+// CSCI 2270 data structues, section 104
+class 
+void mainmenue()
+{
+    cout<< "======Main Menu======"<< endl;
+    cout<< "1. Build Network"<< endl;
+    cout<< "2. Print Network Path"<< endl;
+    cout<< "3. Transmit Message Coast-To-Coast-To-Coast"<< endl;
+    cout<< "4. Add City"<< endl;
+    cout<< "5. Delete City"<< endl; 
+    cout<< "6. Clear Network"<< endl;
+    cout<< "7. Quit"<< endl;
+}
+int main(int argc, *char argv[])
+{
+    int command;
+    while(true)
+    {
+        command=mainmenue();
+        if(command==1)
+        {
+            
+        }
+        if(command==2)
+        {
+
+        }
+        if(command==3)
+        {
+
+        }
+        if(command==4)
+        {
+
+        }
+        if(command==5)
+        {
+
+        }
+        if(command==6)
+        {
+            
+        }
+        if(command==7)
+        {
+            cout<< "Goodbye!"<< endl;
+        }
+
+    }
+}
