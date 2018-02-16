fbca14c5213313d937dcc1572c285def3beb2589
swiftsong <chso8299@colorado.edu>
2018-02-15 20:18:39 -0700

    created class .cpp file

--- data_structures/a4/CommunicationNetwork.cpp
+++ data_structures/a4/CommunicationNetwork.cpp
@@ -1,2 +1,34 @@
 #include "CommunicationNetwork.h"
+// Author: Christian F. Sousa
+// CSCI 2270 data structures section 104
+CommunicationNetwork::CommunicationNetwork(){}
+// this is the default constructor
+CommunicationNetwork::~CommunicationNetwork()
+{
 
+}
+// this is the deconstructor I have made
+void CommunicationNetwork::addcity(string prev, string cityn)
+{
+
+}
+void CommunicationNetwork::buildNetwork()
+{
+
+} 
+void CommunicationNetwork::transmitMsg(char * argv[1])
+{
+
+}
+void CommunicationNetwork::printNetwork()
+{
+
+}
+void CommunicationNetwork::deleteCity(string target)
+{
+    
+}
+void CommunicationNetwork::deleteNetwork()
+{
+
+}
