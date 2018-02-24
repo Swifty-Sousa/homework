fea98279781fe50371409265f47cbadea5e4593a
swiftsong <chso8299@colorado.edu>
2018-02-23 22:56:43 -0700

    updated

--- data_structures/a5/Queue.cpp
+++ data_structures/a5/Queue.cpp
@@ -1,10 +1,59 @@
 #include"Queue.h"
+#include<iostream>
 
 Queue::Queue(int num)
 {
-    queuesize=num;
-    queueHead=0;
-    queueTail=0;
-    queuecount=0;
+    queueSize=num;//  maximum size of the Queue
+    queueHead=0;//index for the beginning of the Queue
+    queueTail=0;// index for the tail of the queue
+    queueCount=0;// count of current items in the Queue
     arrayQueue= new string[num];
+}
+Queue::~Queue()
+{
+    delete arrayQueue;
+    arrayQueue=NULL;
+}
+bool Queue::queueIsEmpty()
+{
+    if(queueCount==0)
+    {
+        return true;
+    }
+    else
+    return false;
+}
+bool Queue::queueIsFull()
+{
+    if(queueCount==queueSize)
+    {
+        return true;
+    }
+    return false;
+}
+void Queue::printQueue()
+{
+    if(queueIsEmpty())
+    {
+        cout<< "Empty"<< endl;
+    }
+}
+void Queue::enqueue()
+{
+
+}
+void Queue::enqueueSentence()
+{
+    if(queueIsFull())
+    {
+        cout<< "Queue is full"<< endl;
+    }
+}
+void Queue::dequeue()
+{
+    if(queueIsEmpty())
+    {
+        cout<<"Queue si empty"<< endl;
+        return;
+    }
 }
