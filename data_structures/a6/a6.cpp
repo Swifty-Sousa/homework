d6e3003dd89ae22b9f70904ab420b8e96dd94e20
Swifty-Sousa <chso8299@colorado.edu>
2018-03-11 01:59:38 -0700

    worked one delete nodes

--- data_structures/a6/a6.cpp
+++ data_structures/a6/a6.cpp
@@ -77,14 +77,244 @@ void deleteLL(MovieNodeBST * head)
     delete temp;
 }
 //we will see
-
-
-
+MovieNodeBST * MovieTree::treeMinimum(MovieNodeBST * rnode)
+{
+    rnode=rnode->rightChild;
+    while(rnode->leftChild!=NULL)
+    {
+        rnode=rnode->leftChild;
+    }
+    return rnode;
+} 
+int deleteBSTnode(MovieNodeBST * node, MovieNodeBST * root)
+{
+	// child
+    MovieNodeBST * minimum= treeMinimum(node);
+    // it hurts that we have to account for this.
+    if(node==root)
+    {
+        if(node->rightChild==NULL && node->leftChild== NULL)
+        {
+			delete node;
+			return 0;
+        }
+		else if(node->rightChild==NULL && node->leftChild!=NULL)
+		{
+			root=node->leftChild;
+			root->parent==NULL;
+			delete node;
+			return 0;
+		}
+		else if(node->rightChild!=NULL && node->leftChild==NULL)
+		{
+			root=node->rightChild;
+			root->parent=NULL;
+			delete node;
+			return 0;
+		}
+		else
+		{
+			if(root->rightChild==minimum)	
+			{
+				root->leftChild->parent=root->rightChild;
+				root=root->rightChild;
+				root->parent=NULL;
+				delete node;
+				return 0;
+			}
+			else if(minimum->rightChild==NULL)
+			{
+				minimum->parent->leftChild=NULL;
+				minimum->parent=NULL;
+				minimum->leftChild=root->leftChild;
+				root->leftChild->parent=minimum;
+				minimum->rightChild=root->rightChild;
+				minimum->rightChild->parent=minimum;
+				root=minimum;
+				delete node;
+				return 0;
+			}
+			else
+			{
+				minimum->rightChild->parent=minimum->parent;
+				minimum->rightChild=minimum->parent->leftChild;
+				minimum->parent=NULL;
+				minimum->rightChild=root->rightChild;
+				root->rightChild->parent=minimum;
+				minimum->leftChild=root->leftChild;
+				root->leftChild->parent=minimum;
+				minimum=root;
+				delete node;
+				return 0;
+			}
+		}
+    }
+    // if the node to be deleted has no child
+    if(node->rightChild==NULL && node->leftChild== NULL)
+    {
+        if(node->parent->leftChild== node)
+        {
+            node->parent->leftChild=NULL;
+            delete node;
+            return 0;
+        }
+        else
+        {
+            node->parent->rightChild=NULL;
+            delete node;
+            return 0;
+        } 
+    }
+    // if the node to be deleted has a leftchild but not a rightchild
+    else if(node->rightChild==NULL &&node->leftChild!=NULL)
+    {
+        if(node->parent->leftChild== node)
+        {
+            node->parent->leftChild=node->leftChild;
+            node->leftChild->parent=node->parent;
+            delete node;
+            return 0;
+        }
+        else
+        {
+            node->parent->rightChild=node->leftChild;
+            node->leftChild->parent=node->parent;
+            delete node;
+            return 0;
+        }    
+    }
+    //if the node to be deleted has a rightchild but not a leftchild
+    else if(node->rightChild!=NULL &&node->leftChild==NULL)
+    {
+        if(node->parent->leftChild== node)
+        {
+            node->parent->leftChild=node->rightChild;
+            node->rightChild->parent=node->parent;
+            delete node;
+            return 0;
+        }
+        else
+        {
+            node->parent->rightChild=node->rightChild;
+            node->rightChild->parent=node->parent;
+            delete node;
+            return 0;
+        }    
+    }
+    // node to be deleted has both left and right children
+    else
+    {
+        //case one with two children
+        if(node->leftChild==minimum)       
+        {
+            if(node->parent->leftChild=node)
+            {
+                node->parent->leftChild=node->rightChild;
+                node->rightChild->parent=node->parent;
+            }
+            else
+            {
+                node->parent->rightChild=node->rightChild;
+                node->rightChild->parent=node->parent;
+            }
+            minimum->leftChild=node->leftChild;
+            node->leftChild->parent=minimum;
+            delete node;
+            return 0;
+        }
+        //case two with two children
+        else if(minimum->rightChild==NULL)
+        {
+            if(node->parent->leftChild=node)
+            {
+                node->parent->leftChild=minimum;
+                minimum->parent=node->parent;
+            }
+            else
+            {
+                node->parent->rightChild=minimum;
+                minimum->parent=node->parent;
+            }
+			minimum->parent->leftChild=NULL;//as to not create and infinte loop when searching 
+			minimum->rightChild=node->rightChild;
+			node->rightChild->parent=minimum;
+			minimum->leftChild=node->leftChild;
+			node->leftChild->parent=minimum;
+			delete node;
+			return 0;
+        }
+		// case 3 with two childrem
+		else
+		{
+			minimum->parent->leftChild=minimum->rightChild;
+			minimum->rightChild->parent=minimum->parent;
+            if(node->parent->leftChild=node)
+            {
+                node->parent->leftChild=minimum;
+                minimum->parent=node->parent;
+            }
+            else
+            {
+                node->parent->rightChild=minimum;
+                minimum->parent=node->parent;
+            }
+			minimum->rightChild=node->rightChild;
+			node->rightChild->parent=minimum;
+			minimum->leftChild=node->leftChild;
+			node->leftChild->parent=minimum;
+			return 0;
+		}
+    }
+}
 
 void MovieTree::deleteMovieNode(string title)
 {
-    char letter =title[0];
-
+    MovieNodeBST * holder= searchBST(root, title);
+    if(holder==NULL)
+    {
+        cout<< "Movie not found."<< endl;
+        return;
+    }
+    else if (holder->head->next==NULL)
+    {
+        deleteBSTnode(holder, root);
+    }
+    MovieNodeLL* tail=holder->head;
+    MovieNodeLL* tip =tail-> next;
+    if(holder->head->title==title)
+    {
+        holder->head=tip;
+        delete tail;
+        return;
+    }
+    bool found=false;
+    while(tip!=NULL)
+    {
+        if(tip->title==title)
+        {
+            found==true;
+            break;
+        }
+        tail==tip;
+        tip==tip->next;
+    }
+    if(!found)
+    {
+        cout<<"Movie not found." << endl;
+        return;
+    }
+    else if(tip->next ==NULL)
+    {
+        delete tip;
+        return;
+    }
+    else
+    {
+        tail->next=tip->next;
+        tip->next =NULL;
+        delete tip;
+        return;
+    }
 }
 void MovieTree::addMovieNode(int ranking, string title, int ry, int q)
 {
@@ -141,10 +371,6 @@ void MovieTree::printMovieInventory(MovieNodeBST * node)
 {
 
 }
-void MovieTree::countMovieNodes(MovieNodeBST * , int *c)
-{
-
-}
 MovieNodeBST* MovieTree::searchBST(MovieNodeBST * node, string title)
 {
     char t=title[0];
@@ -183,12 +409,3 @@ MovieNodeBST *  MovieTree::searchLL(MovieNodeLL * head, string target)
     }
     return NULL;
 }
-* MovieNodeBST MovieTree::treeMinimum(MovieNodeBST * rnode)
-{
-    while(rnode->leftchild!=NULL)
-    {
-        rnode=rnode->leftchild;
-    }
-    return rnode;
-}
-
--- data_structures/a6/main.cpp
+++ data_structures/a6/main.cpp
@@ -27,15 +27,55 @@ int main(void)
     while(true)
     {
         command=menu();
+        cout<<endl;
+        cout<< endl;
+        cout<< command<< endl;
+        cout<< endl;
+        cout<< endl;
         if(command==1)
         {
             cout << "Enter title"<<endl;
+            getchar();
+            getl//Author: Christian F. Sousa
+// CSCI 2270 Data Structures: section 104
+#include<iostream>
+#include"MovieTree.hpp"
+using namespace std;
+
+int menu()
+{
+    int command;
+    //couts
+    cout<< "======Main Menu======"<< endl;
+    cout<< "1. Find Movie"<< endl;
+    cout<< "2. Rent Movie"<< endl;
+    cout<< "3. Print the inventory"<< endl;
+    cout<< "4. Delete a movie" << endl;
+    cout<< "5. Count the movies"<< endl;
+    cout<< "6. Quit"<< endl;
+    cin>> command;
+    return command;
+}
+
+int main(void)
+{
+    MovieTree a;
+    int command;
+    string title;
+    while(true)
+    {
+        command=menu();
+        if(command==1)
+        {
+            cout << "Enter title"<<endl;
+            getchar();
             getline(cin, title);
             a.findMovie(title);
         }
         else if(command==2)
         {
             cout<< "Enter Title"<< endl;
+            getchar();
             getline(cin, title);
         }
         else if(command==3)
@@ -45,6 +85,38 @@ int main(void)
         else if(command==4)
         {
             cout<<"Enter title"<< endl;
+            getchar();
+            getline(cin, title);
+            a.deleteMovieNode(title);
+        }
+        else if(command==5)
+        {
+            cout<<"Tree contains: "<<a.countMovieNodes()<< " movies"<< endl;
+        }
+        else if(command==6)
+        {
+            cout<< "Goodbye!"<< endl;
+            return 0;
+        }
+    }
+}
+ine(cin, title);
+            a.findMovie(title);
+        }
+        else if(command==2)
+        {
+            cout<< "Enter Title"<< endl;
+            getchar();
+            getline(cin, title);
+        }
+        else if(command==3)
+        {
+            a.printMovieInventory();
+        }
+        else if(command==4)
+        {
+            cout<<"Enter title"<< endl;
+            getchar();
             getline(cin, title);
             a.deleteMovieNode(title);
         }
