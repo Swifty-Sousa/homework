3072e7ed4c1e9f23e18600c7c8c48002382c4dad
Swifty-Sousa <chso8299@colorado.edu>
2018-03-20 21:30:34 -0600

    finished r10

--- data_structures/r10/main.cpp
+++ data_structures/r10/main.cpp
@@ -3,22 +3,59 @@
 #include<fstream>
 #include<sstream>
 #include"Graph.h"
-
+using namespace std;
 int main(void)
 {
+    Graph a;
+    string holder;
     string filename="mat.txt";
     ifstream datafile;
+    vector<string>name;
     datafile.open(filename);
     if(datafile.fail())
     {
         cout<< "you done did a goof"<< endl;
     }
     string line;
-    stringstream ss;
-    while(!datafile.eof())
+    string c;
+    getline(datafile, holder);
+    //cout<<holder<< endl;
+    istringstream ss(holder);
+    while(getline(ss,c,' '))
     {
-        getline(datafile,line);
-        ss<<line;
-        while
+        //cout<< holder<<endl;
+        name.push_back(c);
     }
+    int n=1;
+    while(n<name.size())
+    {
+        a.addVertex(name[n]);
+        //cout<< name[n]<< endl;
+        n++;
+    }
+    n=1;
+    while(getline(datafile,holder))
+    {
+        istringstream iss(holder);
+        string s;
+        vector<string> Vtemp;
+        while (getline(iss, s, ' ')) 
+        {
+            Vtemp.push_back(s);
+        }
+        while(n<Vtemp.size())
+        {
+            cout<< Vtemp[n]<< endl;
+            int Ntemp= stoi(Vtemp[n]);
+            if(Ntemp>0)
+            {
+                a.addEdge(name[n],Vtemp[0],Ntemp);
+            }
+            n++;
+        }
+        n=1;
+    }
+    //a.addVertex("boulder");
+    a.displayEdges();
+    return 0;
 }
