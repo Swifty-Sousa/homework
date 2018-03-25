9162bd3563da9834ba1a42eaebd38b72f4b69c30
Swifty-Sousa <chso8299@colorado.edu>
2018-03-25 10:15:35 -0600

    fixed a few bugs

--- data_structures/a8/Graph.cpp
+++ data_structures/a8/Graph.cpp
@@ -24,11 +24,6 @@ void Graph::addEdge(string v1, string v2, int weight){
                     av.v = &vertices[j];
                     av.weight = weight;
                     vertices[i].adj.push_back(av);
-                    //another vertex for edge in other direction
-                    adjVertex av2;
-                    av2.v = &vertices[i];
-                    av2.weight = weight;
-                    vertices[j].adj.push_back(av2);
                 }
             }
         }
@@ -39,7 +34,7 @@ void Graph::addVertex(string n){
     for(int i = 0; i < vertices.size(); i++){
         if(vertices[i].name == n){
             found = true;
-            cout<<vertices[i].name<<" found."<<endl;
+            cout<<vertices[i].name<<" already in the graph."<<endl;
         }
     }
     if(found == false){
@@ -49,13 +44,36 @@ void Graph::addVertex(string n){
 
     }
 }
-
+int Graph::isAdjacent(string one, string two) {
+    for (int i = 0; i < vertices.size(); i++) {
+        if (vertices[i].name == one)
+         {
+            if (two== vertices[i + 1].name) 
+            {
+                return 1;
+            }
+             else if (two== vertices[i - 1].name) 
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 void Graph::displayEdges(){
     //loop through all vertices and adjacent vertices
     for(int i = 0; i < vertices.size(); i++){
         cout<<vertices[i].name<<"-->";
-        for(int j = 0; j < vertices[i].adj.size(); j++){
+        for(int j = 0; j < vertices[i].adj.size(); j++)
+        {
+            if(j<vertices[i].adj.size()-1)
+            {
             cout<<vertices[i].adj[j].v->name<<"***";
+            }
+            else
+            {
+            cout<<vertices[i].adj[j].v->name;
+            }
         }
         cout<<endl;
     }
