917fcf0b8095193fc7bee86727c40c115cb538d9
Swifty-Sousa <chso8299@colorado.edu>
2018-04-24 23:59:03 -0600

    fixed a1

--- data_structures/a1/take2.cpp
+++ data_structures/a1/take2.cpp
@@ -3,78 +3,88 @@
 #include<stdlib.h>
 #include<sstream>
 using namespace std;
-
 struct item
 {
     int status; // one for wanted, two for selling
     string name;
     int price;
     item(){}
-    item(stirng na, int s, int )
-}
+    item(string na, int s, int p)
+    {
+        name=na;
+        status=s;
+        price=p;
+    }
+
+};
 // reutrns true if the object is removable, false if it gets put into the objcts array;
 void sell(item buyer, item seller, int pos, item objects[])
 {
-    cout<<seller->name<< " "<< seller->price<< endl; 
-    objects[pos]==NULL; 
+    cout<<seller.name<< " "<< seller.price<< endl; 
+    item a("", -1, -1);
+    objects[pos]= a; 
     // now do the array shifting so that there are no holes in the array
     for(int i=pos; i<98; i++)
     {
         objects[i]=objects[i+1];
     }
 }
-bool process(string line, item objects[], int pos);
+bool process(string line, item objects[], int pos)
 {
-    stringstream ss(line, ",");
-    stiring holder;
-    string name;
-    int p;
-    string s;
-    getline(ss,holder);
-    name=holder;
-    getline(ss,holder);
-    s=holder;
-    getline(ss, holder);
+    string holder;
+    stringstream ss(line);
+    string nam;
+    string p;
+    int s;
+    getline(ss,holder, ',');
+    nam=holder;
+    getline(ss,holder, ',');
     if(holder== " wanted")
     {
-        p=1;
+        s=1;
     }
     else
     {
-        p=2;
+        s=2;
     }
-    item a(name,s,p);
+    getline(ss, holder, ',');
+    p=holder;
+    item a;
+    a.name=nam;
+    a.status=s;
+    //cout<< "[" <<p<< "]"<< endl;
+    a.price=stoi(p);
     int index;
     for(int i=0; i< pos; i++)
     {
-        if(objcts[i]->name==a->name)
+        if(objects[i].name==a.name)
         {
             //objects have the same name
-            if(objects[i]->status+a->status==3)
+            if(objects[i].status+a.status==3)
             {
                 // one object is for sale the other is wanted.
-                if(objects[i]->status==1)
+                if(objects[i].status==1)
                 {
-                    if(objects[i]->price>=a->price)
+                    if(objects[i].price>=a.price)
                     {
                         //checking prices match
-                        sell(objects[i],a, i,objects[]); 
+                        sell(objects[i],a, i,objects); 
                         return true;
                     }
                 }
                 else
                 {
-                    if(a->price>=objects[i])
+                    if(a.price>=objects[i].price)
                     {
                         //checking price matches
-                        sell(a, objects[i],i,objects[]);
+                        sell(a, objects[i],i,objects);
                         return true;
                     }
                 }
             }
         }
     }
-
+    objects[pos]=a;
     return false;
 }
 int main(int argc, char * argv[])
@@ -84,15 +94,18 @@ int main(int argc, char * argv[])
         cout<<"program requires a filename"<< endl;
     }
     item objects[100];
-    int a(NULL, -1, -1);
+    item a;
+    a.name="";
+    a.status=-1;
+    a.price=-1;
     for(int i=0; i<100;i++)
     {
         objects[i]=a;
     }
     string filename=argv[1];
     ifstream datafile;
-    datafile.open(fielname);
-    if(datafie.fail())
+    datafile.open(filename);
+    if(datafile.fail())
     {
         cout<< "File does not exist in current directory"<< endl;
     }
@@ -100,7 +113,7 @@ int main(int argc, char * argv[])
     int i=0;
     while(getline(datafile,line))
     {
-        if(!process(line, objects[], i))
+        if(!process(line, objects, i))
         {
             i++;
         }
@@ -109,4 +122,20 @@ int main(int argc, char * argv[])
             i--;
         }
     }
+    //cout<< endl;
+    cout<< "#"<< endl;
+    //cout<< endl;
+
+    for(int k=0; k<i; k++)
+    {
+        if(objects[k].status==1)
+        {
+            cout<< objects[k].name<< ", wanted, "<< objects[k].price<<endl;
+        }
+        else
+        {
+            cout<< objects[k].name<< ", for sale, "<< objects[k].price<<endl;
+        }
+    }
+    cout<< "#"<< endl;
 }
